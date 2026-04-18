ab1268b748dbf7ed0830807bf1118934d4542f7f
dhruvaparnathi <dhruv.aparnathi.1@gmail.com>
2026-04-18 21:59:30 +0530

    day2

--- day2/app.js
+++ day2/app.js
@@ -2,4 +2,15 @@ const express = require("express");
 
 const app = express();
 
+app.get("/",(req,res)=>{
+    res.send("Backend is working at port:3000");
+})
+app.get("/about",(req,res)=>{
+    res.send("This is About Page");
+})
+app.get("/home",(req,res)=>{
+    res.send("This is Home Page");
+})
+
+
 app.listen(3000);
